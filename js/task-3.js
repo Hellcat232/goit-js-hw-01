5495b18385ea632b498c1773cf82faa11376ec2f
Hellcat232 <maxkuich1993@gmail.com>
2023-11-16 10:45:49 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,16 @@
-// function getElementWidth(content, padding, border) {
-//   let getElementWidth = `${content + padding + padding + border + border}`;
-//   return getElementWidth;
-// }
-// getElementWidth("50px", "8px", "4px");
-// getElementWidth("60px", "12px", "8.5px");
-// getElementWidth("200px", "0px", "0px");
-// console.log(getElementWidth("50px", "8px", "4px"));
-// console.log(getElementWidth("60px", "12px", "8.5px"));
-// console.log(getElementWidth("200px", "0px", "0px"));
+function getElementWidth(content, padding, border) {
+  //   Number.parseInt(content, padding);
+  //   Number.parseFloat(border);
+  let getElementWidth = `${
+    Number.parseInt(content) +
+    Number.parseInt(padding ** 2) +
+    Number.parseFloat(border ** 2)
+  }`;
+  return getElementWidth;
+}
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
